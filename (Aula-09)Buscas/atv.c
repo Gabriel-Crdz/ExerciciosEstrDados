10cab5502e2d5bdf87670bd2cb583b9f3644e645
Gabriel Cardozo <134400952+Gabriel-Crdz@users.noreply.github.com>
2025-11-27 19:44:07 -0300

    Add files via upload

--- (Aula-09)Buscas/atv.c
+++ (Aula-09)Buscas/atv.c
@@ -1,9 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define TAM 10
+int inserirVetor(int *dados, int tam){
+    int i;
+    for(i = 0; i < tam; i++){
+        printf("Informe o %dº elemento: ", i+1);
+        scanf("%d", &dados[i]);
+    }
+    return *(dados);
+}
+int buscaSeq(int *dados, int tam, int valor){
+    int i= 0, pos = -1;
 
+    /* Usando WHILE */
+    // while ((i<tam) && (pos==-1)){
+    //     if (valor == vet[i]) pos = i;
+    //     i++;
+    // }
 
+    /* Usando FOR */
+    for(i = 0; i < TAM; i++){
+        if(valor == dados[i]){
+            pos = i;
+            break;
+        }
+    }
+    
+    return (pos); // Retorna o índice do vetor onde está o valor, ou -1 se não encontrado
+}
 
+int buscaBin(int *vet, int inicio, int fim, int valor){
+    int meio;
+    int pos = -1;
+
+    if(inicio > fim) pos = -1; // Se o início passou do fim, não achou
+    else{
+        meio = (inicio + fim) / 2; // Calcula a posição do meio do vetor
+
+        if (valor == vet[meio]) pos = meio; // Encontrou elemento, retorna posição
+        else if (valor > vet[meio]) pos = buscaBin(vet, meio + 1, fim, valor); // Procura no segundo subvetor
+        else pos = buscaBin(vet, inicio, meio - 1, valor); // Procura no primeiro subvetor
+    }
+
+    return (pos); // Retorna o índice de vet onde valor foi encontrado, ou -1 se não encontrado
+}
 
 int menu(){
     int opc;
@@ -33,6 +73,28 @@ int main(){
     do{
         opc = menu();
         system("clear");
+        switch(opc){
+            case 1: 
+            *dados = inserirVetor(dados, TAM);
+            break;
+            case 2: 
+                printf("+===SEQUENCIAL===\n");
+                printf("|Informe o valor buscado: ");
+                scanf("%d", &valor);
+                pos = buscaSeq(dados, TAM, valor);
+                printf("RESULTADO:\n");
+                printf("Elemento encontrado na posição: %d", pos);
+            break;
+            case 3: 
+                printf("\nInforme o valor buscado: ");
+                scanf("%d", &valor);
+                pos = buscaBin(dados, 0, TAM, valor);
+                printf("RESULTADO:\n");
+                printf("Elemento encontrado na posição: %d", pos);
+            break;
+            default: printf("SAINDO...");
+            break;
+        }
     }while(opc != 0);
     return 0;
 }
